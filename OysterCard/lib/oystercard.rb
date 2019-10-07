58c722e57843ababc6d02ca0679d39d0b432d7aa
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-10-07 15:34:04 +0100

    Step 9

--- OysterCard/lib/oystercard.rb
+++ OysterCard/lib/oystercard.rb
@@ -1,17 +1,33 @@
 class OysterCard
   attr_reader :balance
   CARD_LIMIT = 90
-
+  MINIMUM_AMOUNT = 1
   def initialize(limit=CARD_LIMIT)
     @balance = 0
     @limit = limit
+    @journeyStatus = false
   end
 
   def top_up(money)
-    p @balance
     fail "Maximum balance exceeded" if @balance + money > CARD_LIMIT
-    p @balance
+
     @balance += money
   end
 
+  def deduct(money)
+    @balance -= money
+  end
+
+  def touch_in
+    fail "Need Minimum of £1" if @balance < MINIMUM_AMOUNT
+    @journeyStatus = true
+  end
+
+  def touch_out
+    @journeyStatus = false
+  end
+
+  def in_journey?
+    @journeyStatus
+  end
 end
--- OysterCard/spec/oystercard_spec.rb
+++ OysterCard/spec/oystercard_spec.rb
@@ -11,6 +11,25 @@ describe OysterCard do
    expect(subject.balance).to eq(10)
  end
 
+ it 'can deduct a fare' do
+   subject.top_up(10)
+   subject.deduct(3)
+   expect(subject.balance).to eq(7)
+ end
+
+ it 'can touch in' do
+   subject.top_up(2)
+   subject.touch_in
+   expect(subject.in_journey?).to eq true
+ end
+
+ it 'can touch out' do
+   subject.top_up(2)
+   subject.touch_in
+   subject.touch_out
+   expect(subject.in_journey?).to eq false
+ end
+
  # it 'Exceeds the Card limit' do
  #   max_bal = OysterCard::CARD_LIMIT
  #   subject.top_up(max_bal)
