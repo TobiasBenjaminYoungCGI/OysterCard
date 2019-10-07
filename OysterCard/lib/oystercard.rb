7e895d77b3d998c34bf7196a23efbfb382ab35e9
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-10-07 15:06:55 +0100

    step6

--- OysterCard/lib/oystercard.rb
+++ OysterCard/lib/oystercard.rb
@@ -1,8 +1,17 @@
 class OysterCard
-  attr_accessor :balance
+  attr_reader :balance
+  CARD_LIMIT = 90
 
-  def initalize
+  def initialize(limit=CARD_LIMIT)
     @balance = 0
+    @limit = limit
+  end
+
+  def top_up(money)
+    p @balance
+    fail "Maximum balance exceeded" if @balance + money > CARD_LIMIT
+    p @balance
+    @balance += money
   end
 
 end
--- OysterCard/spec/oystercard_spec.rb
+++ OysterCard/spec/oystercard_spec.rb
@@ -1,9 +1,19 @@
 require 'oystercard'
 
 describe OysterCard do
- it {is_expected.to respond_to :initalize}
 
  it 'has a balance' do
-   expect(subject.initalize).to eq(0)
+   expect(subject.balance).to eq(0)
  end
+
+ it 'can top up the card' do
+   subject.top_up(10)
+   expect(subject.balance).to eq(10)
+ end
+
+ # it 'Exceeds the Card limit' do
+ #   max_bal = OysterCard::CARD_LIMIT
+ #   subject.top_up(max_bal)
+ #   expect(subject.top_up(1)).to raise_error "Maximum balance exceeded"
+ # end
 end
